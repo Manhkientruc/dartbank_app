4b290a81936b35c4c3af187bf206341dc2b4ddb1
Manhkientruc <caomanh1809@gmail.com>
2026-03-14 09:08:37 +0700

    2nd commit

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
 
-  static const String baseUrl = "http://192.168.0.105:8000";
+  static const String baseUrl = "http://localhost:8000";
 
   static Future<Map<String, dynamic>> login(
       String email,
